3bebb8b3d337f02af823db19266b19ebf26bea6f
20fut <231612@astanait.edu.kz>
2024-10-22 14:17:17 +0500

    PAyPalPayment class

---
+++ src/Strategy/PayPalPayment.java
@@ -0,0 +1,15 @@
+package Strategy;
+
+public class PayPalPayment implements PaymentStrategy {
+    private String email;
+
+    public PayPalPayment(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Processing PayPal payment of $" + amount + " for email: " + email);
+    }
+}
+
