ccca35ad6a344231b1e65dbaf2fc0f1687c1c2fe
20fut <231612@astanait.edu.kz>
2024-10-22 21:47:02 +0500

    Concrete shapes

---
+++ src/Visitor/Triangle.java
@@ -0,0 +1,23 @@
+package Visitor;
+
+public class Triangle implements Shape {
+    private double base, height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
+}
