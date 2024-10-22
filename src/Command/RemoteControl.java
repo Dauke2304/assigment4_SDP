9042e628b5cbafa88b05625c4b47def6b6615785
20fut <231612@astanait.edu.kz>
2024-10-22 14:49:20 +0500

    Remote control class and added nocommands as sine of null

---
+++ src/Command/RemoteControl.java
@@ -0,0 +1,50 @@
+package Command;
+
+public class RemoteControl {
+    private Command[] onCommands;
+    private Command[] offCommands;
+    private Command undoCommand;
+
+    public RemoteControl() {
+        onCommands = new Command[7];  // assuming 7 slots
+        offCommands = new Command[7];
+
+        Command noCommand = new NoCommand();
+        for (int i = 0; i < 7; i++) {
+            onCommands[i] = noCommand;
+            offCommands[i] = noCommand;
+        }
+        undoCommand = noCommand;
+    }
+
+    public void setCommand(int slot, Command onCommand, Command offCommand) {
+        onCommands[slot] = onCommand;
+        offCommands[slot] = offCommand;
+    }
+
+    public void onButtonWasPushed(int slot) {
+        onCommands[slot].execute();
+        undoCommand = onCommands[slot];
+    }
+
+    public void offButtonWasPushed(int slot) {
+        offCommands[slot].execute();
+        undoCommand = offCommands[slot];
+    }
+
+    public void undoButtonWasPushed() {
+        undoCommand.undo();
+    }
+}
+class NoCommand implements Command {
+    @Override
+    public void execute() {
+        System.out.println("No command assigned.");
+    }
+
+    @Override
+    public void undo() {
+        System.out.println("No command to undo.");
+    }
+}
+
