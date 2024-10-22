4849d799fc9130864d6a13a9e05b6ebb456beafc
20fut <231612@astanait.edu.kz>
2024-10-22 21:35:43 +0500

    Chat User Chatroom

---
+++ src/Mediator/ChatRoom.java
@@ -0,0 +1,27 @@
+package Mediator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatRoom implements ChatMediator {
+    private List<User> users;
+
+    public ChatRoom() {
+        this.users = new ArrayList<>();
+    }
+
+    @Override
+    public void sendMessage(String message, User user) {
+        for (User u : users) {
+            // Message should not be received by the user sending it
+            if (u != user) {
+                u.receive(message);
+            }
+        }
+    }
+
+    @Override
+    public void addUser(User user) {
+        this.users.add(user);
+    }
+}
